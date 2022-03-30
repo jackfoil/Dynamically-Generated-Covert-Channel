8a2624d8f363ba4e8af3cc1c88eaf3f9b66b8e4d
trojanMail <ray13alexander@gmail.com>
2022-03-30 08:03:29 -0500

    added modified HPS.py

--- ScapyCovertChannel/HostPiggybackStorage.py
+++ ScapyCovertChannel/HostPiggybackStorage.py
@@ -1,12 +1,9 @@
 #! python
 from scapy.all import *
-from heapq import *
+from heapq import heappop, heappush
 from piggybackEncodeDecodeIP import decodeIP
 from math import floor
 conf.promisc = False
-
-def check_seq_num(p: Packet):
-    """Checks the sequence number to ensure its correct."""
 
 
 def check_eof(ip: str) -> bool:
@@ -25,27 +22,31 @@ if __name__ == "__main__":
     done = True
     priority=[]
     expectedValue=0
-
+    pack = 0
+    print("[*] Listening...")
     while(done):
         pkt = sniff(filter="src port 10010",count=1)
+        pack += 1
         done = check_eof(pkt[0][IP].src)
+        #if not done: break
         binary,seq = decodeIP(pkt[0][IP].src)
-
-
+        print("[*] Received Packet",seq)
         heappush(priority, [int(seq), binary])
 
         while(len(priority) > 0 and expectedValue == priority[0][0]):
             covertBin += heappop(priority)[1]
+            #print(covertBin)
             expectedValue=(expectedValue+1)%256
-
-        #SEQUENCE_NUM,drop = check_seq_num(pkt[0])
-
-        # store pkts in buffer
+    print()
+    print("Priority: ",priority)
+    print("Expected Value: ",expectedValue)
+    print("Total Packets Received: {}".format(pack))
 
 
     # convert the binary covert message into bytes
     j = 0
     hexArray=[]
+    print(covertBin)
     for i in range(floor(len(covertBin)/8)): 
         j=i*8
         if(covertBin[j:j+8].zfill(8) == None):
