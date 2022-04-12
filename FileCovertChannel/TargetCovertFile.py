2720ce7cddf0e6ba160dd10e5c6bb6594e126b24
unknown <Peters-account>
2022-04-12 17:27:00 -0500

    wrapped in function to get called by main

--- FileCovertChannel/TargetCovertFile.py
+++ FileCovertChannel/TargetCovertFile.py
@@ -2,41 +2,42 @@ import socket
 from time import sleep
 from binascii import hexlify
 
-# set the port for client connections
-port = 12003
-# create the socket and bind it to the port
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.bind(('', port))
-# listen for clients
-s.listen(1)
+def socketTiming(port, filepath):
+    # set the port for client connections
+    port = port
+    # create the socket and bind it to the port
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(('', port))
+    # listen for clients
+    s.listen(1)
 
-# a client has connected!
-c, addr = s.accept()
+    # a client has connected!
+    c, addr = s.accept()
 
-# timing constants
-ZERO = .025
-ONE = .1
+    # timing constants
+    ZERO = .025
+    ONE = .1
 
 
-# import covert file as binary data
-covertBin = ""
-with open("smile.png", "rb") as f:
-    covert = f.read()
-for line in covert:
-    covertBin += bin(line)[2:].zfill(8)
+    # import covert file as binary data
+    covertBin = ""
+    with open(filepath, "rb") as f:
+        covert = f.read()
+    for line in covert:
+        covertBin += bin(line)[2:].zfill(8)
 
-print(covertBin)
+    print(covertBin)
 
-n = 0
-# send garbage messages at covert rate
-for i in range(len(covertBin)):     # because we are dealing with large covert messages we need to make it modular
-    c.send(".".encode())            # send dots because why not
-    if(covertBin[n] == "0"):
-        sleep(ZERO)
-    else:
-        sleep(ONE)
-    n = (n + 1)%len(covertBin)
+    n = 0
+    # send garbage messages at covert rate
+    for i in range(len(covertBin)):     # because we are dealing with large covert messages we need to make it modular
+        c.send(".".encode())            # send dots because why not
+        if(covertBin[n] == "0"):
+            sleep(ZERO)
+        else:
+            sleep(ONE)
+        n = (n + 1)%len(covertBin)
 
-# send EOF to know when to stop listening
-c.send("EOF".encode())
-c.close()
+    # send EOF to know when to stop listening
+    c.send("EOF".encode())
+    c.close()
